2dbf7f6a7849282b0cd1fc4c874e9d1839b1d39a
Sudip Ghimire <ghimiresdp@gmail.com>
2025-02-16 21:51:53 +0545

    feat: add matrix initialization

--- problem_solving/basic/matrix_multiplication.py
+++ problem_solving/basic/matrix_multiplication.py
@@ -24,5 +24,54 @@ using any third party libraries, so it might be a bit tricky to perform if you
 do not have better understanding of iteration.
 """
 
+
+class Matrix:
+    # __data: list[list[int | float]]
+
+    def __init__(self, data: list[list[int | float]]) -> None:
+        if data.__len__():
+            _len = data[0].__len__()
+
+            # check if matrix is valid: i.e. all columns have same number of rows
+            for col in range(1, data.__len__()):
+                if data[col].__len__() != _len:
+                    raise ValueError("Invalid matrix")
+        self.__data = data
+
+    @property
+    def size(self):
+        if self.__data.__len__() == 0:
+            return (0, 0)
+        return (self.__data[0].__len__(), self.__data.__len__())
+
+    def __str__(self) -> str:
+        if self.__data.__len__():
+            return "|   |"
+        return "\n".join(
+            ["| " + "".join([f"{i:^5d}" for i in row]) + " |" for row in self.__data]
+        )
+
+    def __mul__(self, other: "Matrix"):
+        """
+        This method overloads python's default multiplication operation between
+        two Matrix objects so that we can easily perform `*` operation.
+        """
+        # TODO
+        pass
+
+
 if __name__ == "__main__":
     print("Matrix multiplication")
+
+    # It raises error "Invalid matrix" when uncommented
+    # m = Matrix(
+    #     [
+    #         [1, 2],
+    #         [3,  ],
+    #         [5, 6],
+    #     ]
+    # )
+    m1 = Matrix([[1, 2], [3, 4]])
+    m2 = Matrix([[2, 3], [4, 5]])
+
+    print("result is: \n", m1 * m2)
