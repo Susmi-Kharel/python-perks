a77847d29431e7a3b173d4c917eb838f92b4ae81
Sudip Ghimire <ghimiresdp@gmail.com>
2025-02-19 21:05:54 +0545

    feat: add median documentation

---
+++ problem_solving/basic/median.py
@@ -0,0 +1,17 @@
+"""
+# Median
+--------
+
+Median is a statistical calculation procedure that finds out the number that
+separates the higher half of the numbers from a list with lower half.
+
+Generally, when we have odd number of elements, we have the number in the list,
+however, if there are even number of elements, we have to find the mean of two
+median values.
+
+if x(n) is nth element of the list, then
+
+         |  x((n+1)/2)                  if n is odd
+median = |
+         |  (x(n/2) + x((n+1)/2))/2     if n is even
+"""
